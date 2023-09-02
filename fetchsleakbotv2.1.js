731e934c4fda6e81162318b4e4209384c63fba1f
sleakchat <support@sleak.chat>
2023-09-02 16:43:18 +0200

    dom content loaded eventlistener in fetchsleakbot

--- fetchsleakbotv2.1.js
+++ fetchsleakbotv2.1.js
@@ -1,44 +1,37 @@
-const sleak_htmlUrl = 'https://dev.sleak.chat/sleakbot.html';
-const sleak_jsUrl = 'https://dev.sleak.chat/sleakbot.js';
+const sleak_htmlUrl = "https://dev.sleak.chat/sleakbot.html";
+const sleak_jsUrl = "https://dev.sleak.chat/sleakbot.js";
 
 // Function to append the Sleak HTML document to the body
 function appendSleakHtmlToBody(sleak_html) {
   const sleak_parser = new DOMParser();
-  const sleak_htmlDoc = sleak_parser.parseFromString(sleak_html, 'text/html');
+  const sleak_htmlDoc = sleak_parser.parseFromString(sleak_html, "text/html");
   document.body.appendChild(sleak_htmlDoc.documentElement);
 }
 
 // Function to append the Sleak JS script to the body
 function appendSleakJsToBody() {
-  const sleak_script = document.createElement('script');
+  const sleak_script = document.createElement("script");
   sleak_script.src = sleak_jsUrl;
   document.body.appendChild(sleak_script);
 }
 
-// Function to fetch the Sleak HTML document and the JS script
-function fetchResources() {
-  const htmlPromise = fetch(sleak_htmlUrl)
-    .then(sleak_response => sleak_response.text())
-    .then(sleak_html => {
+// Function to fetch the Sleak HTML document and append it to the DOM
+function fetchAndAppendHtml() {
+  return fetch(sleak_htmlUrl)
+    .then((sleak_response) => sleak_response.text())
+    .then((sleak_html) => {
       appendSleakHtmlToBody(sleak_html);
     });
-
-  const jsPromise = fetch(sleak_jsUrl)
-    .then(() => {
-      appendSleakJsToBody();
-    });
-
-  // Wait for both promises to resolve
-  return Promise.all([htmlPromise, jsPromise]);
 }
 
-// Call the fetchResources function to fetch and append the resources
-fetchResources()
-  .then(() => {
-    // Resources have finished loading and have been appended, perform any necessary actions here
-    // You can add your code here that relies on the Sleak HTML and JS being loaded
-    console.log('Sleak resources have been loaded and appended to the body.');
-  })
-  .catch(error => {
-    console.error('Error occurred while loading resources:', error);
-  });
+// Wait for the DOM content to load and then append the JS script
+document.addEventListener("DOMContentLoaded", () => {
+  fetchAndAppendHtml()
+    .then(() => {
+      appendSleakJsToBody();
+      console.log("Sleak resources have been loaded and appended to the body.");
+    })
+    .catch((error) => {
+      console.error("Error occurred while loading resources:", error);
+    });
+});

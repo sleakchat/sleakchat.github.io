b57d304f01aca79414d20757f3475bc86d467791
Bas Rijnen <133449281+sleakchat@users.noreply.github.com>
2024-01-10 19:16:52 +0100

    Update prompt_1.js

--- mkbdesk/prompt_1.js
+++ mkbdesk/prompt_1.js
@@ -1,13 +1,5 @@
-const bedrijfsnaam = "test";
-const branche = "test";
+test
 
-const a1_1 = "test";
-const a1_2 = "test";
-const a1_3 = "test";
-const a1_4 = "test";
-const a1_5 = "test";
-const a1_6 = "test";
-const a1_7 = "test";
-const a1_8 = "test";
+test
 
-prompt1 = `my prompt`;
+test
